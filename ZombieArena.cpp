19708ff9712d8b036533efb0b5291f7fa5810ef3
amaraoussama94 <wassa36@yahoo.fr>
2022-11-08 08:12:49 +0100

    m&in function bug fixing

--- ZombieArena.cpp
+++ ZombieArena.cpp
@@ -1,227 +1,261 @@
-#include "Player.hpp"
-#include "ZombieArena.hpp"
 #include <SFML/Graphics.hpp>
 
-//using namespace sf;
+#include "ZombieArena.hpp"
+#include "Player.hpp"
+
+using namespace sf;
+
 int main()
-    {
-        // The game will always be in one of four states
-        enum class State { PAUSED, LEVELING_UP,GAME_OVER, PLAYING };
-        // Start with the GAME_OVER state
-        State state = State::PLAYING;//GAME_OVER
-        // Get the screen resolution and
+{
+	// The game will always be in one of four states
+	enum class State { PAUSED, LEVELING_UP, GAME_OVER, PLAYING };
+	// Start with the GAME_OVER state
+	State state = State::GAME_OVER;
 
-        // create an SFML window
-        Vector2f resolution;
-        resolution.x =VideoMode::getDesktopMode().width;
-        resolution.y =VideoMode::getDesktopMode().height;
-        // Create a video mode object
-        RenderWindow window(VideoMode(resolution.x, resolution.y),"Zombie Arena");// Style::Fullscreen
-        // Create a an SFML View for the main action
-        View mainView(sf::FloatRect(0, 0,resolution.x, resolution.y));/*************************************/
-        // Here is our clock for timing everything
-        Clock clock;
-        // How long has the PLAYING state ²been active
-        Time gameTimeTotal;
-        // Where is the mouse in
-        // relation to world coordinates
-        Vector2f mouseWorldPosition;
-        // Where is the mouse in
-        // relation to screen coordinates
-        Vector2i mouseScreenPosition;
-        // Create an instance of the Player class
-        Player player;
-        // The boundaries of the arena
-        IntRect arena;
-        // The main game loop
+	// Get the screen resolution and create an SFML window
+	Vector2f resolution;
+	resolution.x = VideoMode::getDesktopMode().width;
+	resolution.y = VideoMode::getDesktopMode().height;
 
-        // Create the background
-        VertexArray background;
-        // Load the texture for our background vertex array
-        Texture textureBackground;
-        textureBackground.loadFromFile("graphics/background_sheet.png");
+	RenderWindow window(VideoMode(resolution.x, resolution.y),"Zombie Arena");//, Style::Fullscreen
 
-        while (window.isOpen())
-            {
-                /*
-                ************
-                Handle input
-                ************
-                */                
-                Event event;  
-                while (window.pollEvent(event))
-                    {
-                        if (event.type == Event::KeyPressed)
-                            {
-                                // Pause a game while playing
-                                if (event.key.code == Keyboard::Return && state == State::PLAYING)
-                                    {
-                                        state = State::PAUSED;
-                                    }
-                                // Restart while pausedresolution.x, resolution.y
-                                        // Reset the clock so there isn't a frame jump
-                                else if (event.key.code == Keyboard::Return &&state == State::PAUSED)
-                                    {
-                                        state = State::PLAYING;
-                                        // Reset the clock so there isn't a frame jump
-                                        clock.restart();
-                                    }
-                                // Start a new game while in GAME_OVER state
-                                else if (event.key.code == Keyboard::Return &&state == State::GAME_OVER)
-                                    {
-                                        state = State::LEVELING_UP;
-                                    }
-                                if (state == State::PLAYING)
-                                    {
+	// Create a an SFML View for the main action
+	View mainView(sf::FloatRect(0, 0, resolution.x, resolution.y));
 
-                                    }
-                            }
-                    }// End event polling
+	// Here is our clock for timing everything
+	Clock clock;
+	// How long has the PLAYING state been active
+	Time gameTimeTotal;
 
-                // Handle the player quitting
-                if (Keyboard::isKeyPressed(Keyboard::Escape))
-                    {
-                        window.close();
-                    }
-                    
-                // Handle ZQSD while playing
-                if (state == State::PLAYING)
-                    {
-                        // Handle the pressing and releasing of the ZQSD keys
-                        if (Keyboard::isKeyPressed(Keyboard::Z))
-                            {
-                                player.moveUp();
-                            }
-                        else
-                            {
-                                player.stopUp();
-                            }
-                        if (Keyboard::isKeyPressed(Keyboard::S))
-                            {
-                                player.moveDown();
-                            }
-                        else
-                            {
-                                player.stopDown();
-                            }
-                        if (Keyboard::isKeyPressed(Keyboard::Q))
-                            {
-                                player.moveLeft();
-                            }
-                        else
-                            {
-                                player.stopLeft();
-                            }
-                        if (Keyboard::isKeyPressed(Keyboard::D))
-                            {
-                                player.moveRight();
-                            }
-                        else
-                            {
-                                player.stopRight();
-                            }
-                    }// End ZQSD while playing
-                // Handle the LEVELING up
-                if (state == State::LEVELING_UP)
-                    {                 
-                        if (event.key.code == Keyboard::Num1)
-                            {
-                                state = State::PLAYING;
-                            }
-                        if (event.key.code == Keyboard::Num2)
-                            {
-                                state = State::PLAYING;
-                            }
-                        if (event.key.code == Keyboard::Num3)
-                            {
-                                state = State::PLAYING;
-                            }
-                        if (event.key.code == Keyboard::Num4)
-                            {
-                                state = State::PLAYING;
-                            }
-                        if (event.key.code == Keyboard::Num5)
-                            {
-                                state = State::PLAYING;
-                            }
-                        if (event.key.code == Keyboard::Num6)
-                            {
-                                state = State::PLAYING;
-                            }
-                        if (state == State::PLAYING)
-                            {
-                                // Prepare the level
-                                // We will modify the next two lines later
-                                arena.width = 500;
-                                arena.height = 500;
-                                arena.left = 0;
-                                arena.top = 0;
-                                // Pass the vertex array by reference
-                                // to the createBackground function
-                                int tileSize = createBackground(background, arena);
-                                // We will modify this line of code later
-                                //int tileSize = 50;
-                                // Spawn the player in the middle of the arena
-                                player.spawn(arena, resolution, tileSize);
-                                // Reset the clock so there isn't a frame jump
-                                clock.restart();
-                            }
-                    }// End LEVELING up
-                /*
-                ****************
-                UPDATE THE FRAME
-                ****************
-                */
-                if (state == State::PLAYING)
-                    {
-                        // Update the delta time
-                        Time dt = clock.restart();
-                        // Update the total game time
-                        gameTimeTotal += dt;
-                        // Make a decimal fraction of 1 from the delta time
-                        float dtAsSeconds = dt.asSeconds();
-                        // Where is the mouse pointer
-                        mouseScreenPosition = Mouse::getPosition();
-                        // Convert mouse position to world coordinates of mainView
-                        mouseWorldPosition = window.mapPixelToCoords(
-                        Mouse::getPosition(), mainView);
-                        // Update the player
-                        player.update(dtAsSeconds, Mouse::getPosition());
-                        // Make a note of the players new position
-                        Vector2f playerPosition(player.getCenter());
-                        // Make the view centre around the player
-                        mainView.setCenter(player.getCenter());
-                    }// End updating the scene
-                /*
-                **************
-                Draw the scene
-                **************
-                */
-                if (state == State::PLAYING)
-                    {
-                        window.clear();
-                        // set the mainView to be displayed in the window
-                        // And draw everything related to it
-                        window.setView(mainView);
+	// Where is the mouse in relation to world coordinates
+	Vector2f mouseWorldPosition;
+	// Where is the mouse in relation to screen coordinates
+	Vector2i mouseScreenPosition;
 
-                        // Draw the background
-                        window.draw(background, &textureBackground);
+	// Create an instance of the Player class
+	Player player;
 
-                        // Draw the player
-                        window.draw(player.getSprite());
-                    }
-                if (state == State::LEVELING_UP)
-                    {
-                    }
-                if (state == State::PAUSED)
-                    {
-                    }
-                if (state == State::GAME_OVER)
-                    {
-                    }
-                window.display();
+	// The boundaries of the arena
+	IntRect arena;
 
-            }// End game loop
+	// Create the background
+	VertexArray background;
+	// Load the texture for our background vertex array
+	Texture textureBackground;
+	textureBackground.loadFromFile("graphics/background_sheet.png");
 
-        return 0;
-    }
+	// The main game loop
+	while (window.isOpen())
+	{
+		/*
+		************
+		Handle input
+		************
+		*/
+
+		// Handle events
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::KeyPressed)
+			{
+				// Pause a game while playing
+				if (event.key.code == Keyboard::Return && state == State::PLAYING)
+				{
+					state = State::PAUSED;
+				}
+
+				// Restart while paused
+				else if (event.key.code == Keyboard::Return && state == State::PAUSED)
+				{
+					state = State::PLAYING;
+					// Reset the clock so there isn't a frame jump
+					clock.restart();
+				}
+
+				// Start a new game while in GAME_OVER state
+				else if (event.key.code == Keyboard::Return && state == State::GAME_OVER)
+				{
+					state = State::LEVELING_UP;
+				}
+
+				if (state == State::PLAYING)
+				{
+				}
+
+			}
+		}// End event polling
+
+
+		 // Handle the player quitting
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		// Handle controls while playing
+		if (state == State::PLAYING)
+		{
+			// Handle the pressing and releasing of the ZQSD keys
+			if (Keyboard::isKeyPressed(Keyboard::Z))
+			{
+				player.moveUp();
+			}
+			else
+			{
+				player.stopUp();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::S))
+			{
+				player.moveDown();
+			}
+			else
+			{
+				player.stopDown();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::Q))
+			{
+				player.moveLeft();
+			}
+			else
+			{
+				player.stopLeft();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::D))
+			{
+				player.moveRight();
+			}
+			else
+			{
+				player.stopRight();
+			}
+
+		}// End ZQSD while playing
+
+		 // Handle the levelling up state
+		if (state == State::LEVELING_UP)
+		{
+			// Handle the player levelling up
+			if (event.key.code == Keyboard::Num1)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num2)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num3)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num4)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num5)
+			{
+				state = State::PLAYING;
+			}
+
+			if (event.key.code == Keyboard::Num6)
+			{
+				state = State::PLAYING;
+			}
+
+			if (state == State::PLAYING)
+			{
+				// Prepare thelevel
+				// We will modify the next two lines later
+				arena.width = 500;
+				arena.height = 500;
+				arena.left = 0;
+				arena.top = 0;
+
+				// Pass the vertex array by reference 
+				// to the createBackground function
+				int tileSize = createBackground(background, arena);
+				
+				// Spawn the player in the middle of the arena
+				player.spawn(arena, resolution, tileSize);
+
+				// Reset the clock so there isn't a frame jump
+				clock.restart();
+			}
+		}// End levelling up
+
+		 /*
+		 ****************
+		 UPDATE THE FRAME
+		 ****************
+		 */
+		if (state == State::PLAYING)
+		{
+			// Update the delta time
+			Time dt = clock.restart();
+			// Update the total game time
+			gameTimeTotal += dt;
+			// Make a decimal fraction of 1 from the delta time
+			float dtAsSeconds = dt.asSeconds();
+
+			// Where is the mouse pointer
+			mouseScreenPosition = Mouse::getPosition();
+
+			// Convert mouse position to world coordinates of mainView
+			mouseWorldPosition = window.mapPixelToCoords(Mouse::getPosition(), mainView);
+
+			// Update the player
+			player.update(dtAsSeconds, Mouse::getPosition());
+
+			// Make a note of the players new position
+			Vector2f playerPosition(player.getCenter());
+
+			// Make the view centre around the player				
+			mainView.setCenter(player.getCenter());
+		}// End updating the scene
+
+		 /*
+		 **************
+		 Draw the scene
+		 **************
+		 */
+
+		if (state == State::PLAYING)
+		{
+			window.clear();
+
+			// set the mainView to be displayed in the window
+			// And draw everything related to it
+			window.setView(mainView);
+
+			// Draw the background
+			window.draw(background, &textureBackground);
+
+			// Draw the player
+			window.draw(player.getSprite());
+		}
+
+		if (state == State::LEVELING_UP)
+		{
+		}
+
+		if (state == State::PAUSED)
+		{
+		}
+
+		if (state == State::GAME_OVER)
+		{
+		}
+
+		window.display();
+
+	}// End game loop
+
+	return 0;
+}
